e2ae64199ab7e8fd2f4cb5879a69e90cc662297b
miton18 <rcd18@hotmail.fr>
2016-05-06 23:45:53 +0200

    Logger ma geule

--- api/controller/Logger.php
+++ api/controller/Logger.php
@@ -8,6 +8,9 @@
 
 namespace Controllers;
 
+use \DateTime;
+use \Models\Log;
+
 /**
  * Class Logger
  * @package Controllers
@@ -16,18 +19,26 @@ class Logger
 {
 
     /**
+     * [PRIVATE]
      *  Register a message in log table with a type and a message
+     * @param $type
+     * @param $msg
+     *
+     * @return array
      */
-    public static function log( $type, $msg )
+    private static function log( $type, $msg )
     {
-        
+        $log = new Log( $type , $msg );
+        return $log->save();
     }
 
     /**
      * Log an information
      * @param $msq
+     *
+     * @return array
      */
-    public static function info ($msq)
+    public static function info( $msq )
     {
         return self::log( 'info', $msq );
     }
@@ -35,8 +46,10 @@ class Logger
     /**
      * Log a warning
      * @param $msq
+     *
+     * @return array
      */
-    public static function warn ($msq)
+    public static function warn( $msq )
     {
         return self::log( 'warning', $msq );
     }
@@ -44,9 +57,45 @@ class Logger
     /**
      * Log an error
      * @param $msq
+     *
+     * @return array
      */
-    public static function error ($msq)
+    public static function error( $msq )
     {
         return self::log( 'error', $msq );
+    }
+
+    /**
+     * Look for all logs between 2 dates
+     * @param $start
+     * @param $end
+     *
+     * @return array
+     */
+    public static function getBetweenDate( $start, $end )
+    {
+        $startTimestamp = new DateTime($start);
+        $endTimestamp   = new DateTime($end);
+
+        $res = Log::searchBetween( $startTimestamp->getTimestamp(), $endTimestamp->getTimestamp() );
+        
+        return var_dump($res);
+    }
+
+    /**
+     * Return the last n logs, default is 20
+     * @param int $number
+     *
+     * @return mixed
+     */
+    public static function getLastLogs( $number=20 )
+    {
+        $number = (int) $number;
+        if( !is_int($number) or  $number <= 0 ){
+            return [
+                'err' => 'Mauvais paramètre'
+            ];
+        }
+        return var_dump( Log::searchLastLogs($number) );
     }
 }
--- api/model/Log.php
+++ api/model/Log.php
@@ -9,6 +9,7 @@
 namespace Models;
 
 use \API\Database;
+use \DateTime;
 
 class Log
 {
@@ -16,15 +17,118 @@ class Log
     public $type;
     public $content;
 
-    public function __construct( $type=null, $content=null )
+    public function __construct( $timestamp=null, $type=null, $content=null )
     {
-        $this->type     = empty($type)?     'info'  : $type;
-        $this->content  = empty($content)?  ''      : $content;
+        $this->timestamp    = $timestamp;
+        $this->type         = empty($type)?     'info'  : $type;
+        $this->content      = empty($content)?  ''      : $content;
     }
 
     public function save()
     {
-        
+        $req = Database::getInstance()->PDOInstance->prepare(
+            "INSERT INTO log (timestamp, type, content) VALUES (UNIX_TIMESTAMP(), :type, :content)"
+        );
+
+        if( $req->execute([
+            ':type'     => $this->type,
+            ':content'  => $this->content
+        ]))
+        {
+            return ['err' => null];
+        }
+        else {
+            return ['err' => "Impossible d'enregistrer ce log."];
+        }
     }
 
+    public function load()
+    {
+        $res = Database::Select("SELECT * FROM log WHERE timestamp='". $this->timestamp ."'")
+            ->fetchAll( \PDO::FETCH_ASSOC )[0];
+
+        // complète le this avec les valeurs récupérées
+        foreach( $res as $key => $val ) {
+            $this->$key = $val;
+        }
+    }
+
+    /**
+     * Return log in array format
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'timestamp' => $this->timestamp,
+            'type'      => $this->type,
+            'content'   => $this->content
+        ];
+    }
+
+    /**
+     * Return log in json format
+     * @return mixed
+     */
+    public function toJson()
+    {
+        return json_encode( $this->toArray() );
+    }
+
+    /**
+     * Return a Date object from timestamp
+     * @return $this
+     */
+    public function timestampToDate()
+    {
+        $tmp = new DateTime();
+        return $tmp->setTimestamp( $this->timestamp );
+    }
+
+    /**
+     * Return an array of log Object between 2 dates
+     * @param $a
+     * @param $b
+     *
+     * @return array
+     */
+    public static function searchBetween( $a, $b )
+    {
+        $logs   = [];
+        $req    = Database::Select("SELECT * FROM log WHERE timestamp BETWEEN '". $a ."' AND '". $b ."'");
+
+        foreach ($req as $k) {
+
+            $log = new self( $k->timestamp, $k->type, $k->content );
+            array_push($logs, $log);
+        }
+        return $logs;
+    }
+
+    public static function searchLastLogs( $number )
+    {
+        $logs   = [];
+        $req    = Database::Select("SELECT * FROM log ORDER BY timestamp DESC LIMIT " . $number);
+
+        foreach ($req as $k) {
+
+            $log = new self( $k->timestamp, $k->type, $k->content );
+            array_push($logs, $log);
+        }
+        return $logs;
+    }
+
+    /**
+     * [EXPERIMENTAL]
+     * Apply a filter on an log array, this return a new array of filtered values
+     * @param $logs
+     * @param $type
+     */
+    public static function filterType( $logs, $type ) {
+        foreach( $logs as $k ) {
+            if( $k->type != $type ){
+                unset( $k );
+            }
+        }
+    }
 }
