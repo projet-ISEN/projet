a13dbd00f8d1686612a9922dee9478743208aed1
miton18 <rcd18@hotmail.fr>
2016-05-15 16:19:35 +0200

    end effectifs

--- api/controller/Effectif.php
+++ api/controller/Effectif.php
@@ -38,6 +38,10 @@ class Effectif
         return;
     }
 
+    /**
+     * Return all effectif demand of club
+     * @param $id
+     */
     public function getOne( $id )
     {
         $effectif = new \Models\Effectif( $id );
@@ -46,17 +50,48 @@ class Effectif
         return;
     }
 
+    /**
+     * Set effectif demand for a club, for all projects type
+     * @param $club_id
+     */
     public static function set( $club_id )
     {
+        if( !$_SESSION['user']->isPresident() ) {
+            echo json_encode([
+                'err' => "Vous n'êtes pas autorisé à faire ça"
+            ]);
+            return;
+        }
+
         $put = json_decode( file_get_contents("php://input"), true);
+
         if( empty($put['effectifs']) ){
             echo json_encode([
                'err' => 'Il manque des paramètres'
             ]);
             return;
         }
+        $status = true;
         foreach ( $put['effectifs'] as $effectif ) {
-            var_dump($effectif);
+            //var_dump($effectif);
+            $tmp = new \Models\Effectif($club_id, $effectif['project_id']);
+            $tmp->nb_asked_min = $effectif['nb_asked_min'];
+            $tmp->nb_asked_max = $effectif['nb_asked_max'];
+
+            if( !$tmp->save() ) {
+                $status = false;
+            }
+        }
+
+        if( $status ) {
+            echo json_encode([
+                'err' => null
+            ]);
+        }
+        else {
+            echo json_encode([
+                'err' => 'Un problème est survenu'
+            ]);
         }
         return;
     }
--- api/index.php
+++ api/index.php
@@ -34,8 +34,8 @@
 
         //test variable
 
-        //$userData["Login"]      = 'rcolli17'; $userData["gidNumber"] = "1000";//Evaluator & administrator adn school staf
-        $userData["Login"]      = 'vrioua17';  $userData["gidNumber"] = "1000"; //Evaluator school_staff
+        $userData["Login"]      = 'rcolli17'; $userData["gidNumber"] = "1000";//Evaluator & administrator adn school staf
+        //$userData["Login"]      = 'vrioua17';  $userData["gidNumber"] = "1000"; //Evaluator school_staff
         //$userData["Login"]      = 'pverba17'; //Prez BDE
         //$userData["Login"]      = 'fduboi17'; //trez BDE
         //$userData["Login"]      = 'mgoanv17'; //Capisen Prez
@@ -45,7 +45,7 @@
         //$userData["Login"]      = 'qduche17'; //PA BDE
         //$userData["Login"]      = 'gymorv17'; //PA Capisen
         //$userData["Login"]      = 'gbiann17'; //Evaluator & administrator
-        //$userData["Login"]      = 'mgoanv17';
+        $userData["Login"]      = 'mgoanv17';
 
         $userData["Name"]       = 'prename name'; // prénom nom
         $userData["FirstName"]  = 'prename'; // prénom
@@ -207,5 +207,5 @@ if (date("n")>7) $myYear++;
 
     })->with('id', '[0-9]+')->with('nom', '[A-z\-0-9]+');*/
 
-    //header('Content-Type: application/json; charset=utf-8');
+    header('Content-Type: application/json; charset=utf-8');
     $router->run();
